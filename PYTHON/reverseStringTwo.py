46c199d2db983baa24adb15187d4f4de0e14766d
Woojin Park <jay.seizethemoment@gmail.com>
2020-05-31 18:31:56 -0400

    Create reverseStringTwo.py

---
+++ PYTHON/reverseStringTwo.py
@@ -0,0 +1,9 @@
+## Another vesion of reverseString
+### applying python pre-defined reversed function
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        return "".join(s)
+    
