24512c9059b1a9d4e257a00f4770bf058e5a37d5
Woojin Park <jay.seizethemoment@gmail.com>
2024-04-17 15:47:34 +0900

    Update pray.py

--- PYTHON/pray.py
+++ PYTHON/pray.py
@@ -1,33 +1,60 @@
-def tryyy(y_train) : 
-    p=d=q=range(0,2)
+def tryyy(y_train):
+    p = d = q = range(0, 2)
 
-    pdq=list(itertools.product(p,d,q))
-    seasonal_pdq=[(x[0],x[1],x[2],12) for x in list(itertools.product(p,d,q))]
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
 
     best_aic = np.inf
     best_params = None
-    best_seasonal_parmas = None
+    best_seasonal_params = None
     
     for pdq_params in pdq:
         for seasonal_params in seasonal_pdq:
             try:
-                model=SARIMAX(y_train,
-                          order=pdq_params,
-                          seasonal_order=seasonal_params
-                         )
-                result=model.fit()
-                print("SARIMAX({}x{}12)-AIC:{}".format(pdq_params,
-                                                   seasonal_params,
-                                                   result.aic
-                                                  ))
-                if results.aic < best_aic :
-                    best_aic = results.aic
-                    best_params = param
-                    best_seasonal_parmas = param_seasonal
+                model = SARIMAX(y_train,
+                                order=pdq_params,
+                                seasonal_order=seasonal_params)
+                result = model.fit()
+                print("SARIMAX({}x{})-AIC: {}".format(pdq_params, seasonal_params, result.aic))
+                
+                if result.aic < best_aic:
+                    best_aic = result.aic
+                    best_params = pdq_params
+                    best_seasonal_params = seasonal_params
             except:
                 continue
                 
-    return best_params, best_seasonal_parmas
+    return best_params, best_seasonal_params
 
-best_params, best_seasonal_parmas = tryyy(y_train)
+best_params, best_seasonal_params = tryyy(y_train)
+print(best_params)
+def tryyy(y_train):
+    p = d = q = range(0, 2)
+
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+    best_aic = np.inf
+    best_params = None
+    best_seasonal_params = None
+    
+    for pdq_params in pdq:
+        for seasonal_params in seasonal_pdq:
+            try:
+                model = SARIMAX(y_train,
+                                order=pdq_params,
+                                seasonal_order=seasonal_params)
+                result = model.fit()
+                print("SARIMAX({}x{})-AIC: {}".format(pdq_params, seasonal_params, result.aic))
+                
+                if result.aic < best_aic:
+                    best_aic = result.aic
+                    best_params = pdq_params
+                    best_seasonal_params = seasonal_params
+            except:
+                continue
+                
+    return best_params, best_seasonal_params
+
+best_params, best_seasonal_params = tryyy(y_train)
 print(best_params)
