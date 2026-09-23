9e460067f40b9d4f4fd0301100dc991a90bbb69c
Woojin (Jay) Park <jay.seizethemoment@gmail.com>
2026-09-24 00:02:02 +0900

    Implement article_views function in SQL script
    Added a function to process article views and extract unique authors who viewed their own articles.

--- SQL/Article Views.sql
+++ SQL/Article Views.sql
@@ -6,6 +6,22 @@ ORDER BY id ASC;
 
 
 
+-- 
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    authors_viewed_own_articles = views[views['author_id'] == views['viewer_id']]
+    #unique_authors = authors_viewed_own_articles['author_id'].unique()
+    unique_authors = list(set(authors_viewed_own_articles['author_id'].tolist()))
+    # Sort the unique author_ids in ascending order
+    unique_authors = sorted(unique_authors)
+    # Create a DataFrame with the sorted unique author_ids and rename the 'author_id' column to 'id'
+    result_df = pd.DataFrame({'id': unique_authors})
+    return result_df
+
+ -- 
+
 Table: Views
 
 +---------------+---------+
